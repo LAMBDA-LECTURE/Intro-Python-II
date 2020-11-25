d70443acf38756dbb0482285cdfb90c7db6e6450
Josh Glantz-Hucks <jahteo@gmail.com>
2020-11-25 06:54:51 -0800

    mostly working pre-lecture

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,47 @@
 from room import Room
+from player import Player
+from item import Item
 
 # Declare all the rooms
+item = {
+    "shiny sword": Item("Shiny sword of doom",
+                    "Makes you want to cuddle with it.. "),
+    "bow": Item("Wooden Bow",
+                    "Shoots Arrows of wrath and & vengeance"),
+    "lancer": Item("Lancer",
+                    "It's a lancer. what did you expect?"),
+    "cup": Item("Cup of Everfill",
+                    "No matter how hard you try,it's always full"),
+    "bong": Item("A Bong",
+                    "It's some sort of porcelain art peice..."),
+    "snake": Item("Teddy",
+                    "Stuffed and NOT cuddle"),
+    "potion": Item("of greater healing",
+                    "It's some sort of porcelain art peice..."),
+}
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons",
+                     [item['snake'], item["bow"]]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""",
+                [item["bong"]]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",
+                [item["bong"]]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""",
+                [item["bong"]]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",
+                [item["bong"]]),
 }
 
 
@@ -38,7 +61,6 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +71,49 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+# this is baue's solution code. idk wtf is goiing on.
+player = Player("Josh", room["outside"])
+running = True
+current_room = None
+while(running):
+    print(player.current_room.name)
+    print(player.current_room.description)
+
+    direction = input("where would you like to go? ")
+    if direction == "n" :
+        player.current_room = player.current_room.n_to
+    elif direction == "s" :
+        player.current_room = player.current_room.s_to
+    elif direction == "e" :
+        player.current_room = player.current_room.e_to
+    elif direction == "w" :
+        player.current_room = player.current_room.w_to
+    elif direction == "a" :
+        print(f"you investigate and find {player.current_room.items}")
+        action = input("Would you like any of these items? ")
+        if action == "y":
+            # for index, value in player.current_room.items:
+            #     print(f'[{index}]: {value}')
+            select_item = int(input("which item would you like? "))
+            if len(player.current_room.items) - 1 >= select_item:
+                player.pickup(player.current_room.items[select_item])
+                # player.current_room.steal(player.current_room.items[select_item])
+                player.current_room.steal(items[select_item])
+    elif direction == "d" :
+        print(f"you investigate your pockets and find {player.items}")
+        action = input("Would you like to drop any of these items? ")
+        if action == "y":
+            for index, value in player.items:
+                print(f'[{index}]: {value}')
+            select_item = input("which item would you like to drop? ")
+            if len(player.items) - 1 > select_item:
+                player.drop(player.items[select_item])
+                player.current_room.add(player.items[select_item])
+
+    elif direction == "q":
+        running = False
+    else:
+        print("please ender a cardinal direction or q to exit")
+        # currently errors out if we go to a none existent room:
+    if player.current_room is None:
+        print(f"there is nowhere for {player.name} to go that way")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,23 @@
 # Implement a class to hold room information. This should have name and
-# description attributes.
+# # description attributes.
+# * Put the Room class in `room.py` based on what you see in `adv.py`.
+
+#   * The room should have `name` and `description` attributes.
+
+#   * The room should also have `n_to`, `s_to`, `e_to`, and `w_to` attributes
+#     which point to the room in that respective direction.
+class Room:
+  def __init__(self, name, description, items = []):
+    self.name = name
+    self.description = description
+    self.items = items
+    n_to = None
+    s_to = None
+    e_to = None
+    w_to = None
+  def __repr__(self):
+    return f'{self.items}'
+  def add(self, item):
+    self.items.append(item)
+  def steal(self, item):
+    self.items.remove(item)
